b068ff2e9e7357c73410630a28aa808fceb38245
Formola <formola.paolo@gmail.com>
2025-08-29 12:08:13 +0200

    settembre-2024

--- settembre-2024/main.c
+++ settembre-2024/main.c
@@ -18,86 +18,20 @@ Realizzare in MPI C un programma in cui:
 #include <mpi.h>
 
 #define FILE_NAME "input.txt"
+#define DIM 12
 
-int *read_vector_from_file(const char *filename, int *dim)
+int print_vector(int *v, int dim)
 {
-    FILE *file = fopen(filename, "r");
-    if (file == NULL)
-    {
-        perror("Error opening file");
-        return NULL;
-    }
-    fscanf(file, "%d", dim);
-    int *vector = malloc(*dim * sizeof(int));
-    for (int i = 0; i < *dim; i++)
-    {
-        fscanf(file, "%d", &vector[i]);
-    }
-    fclose(file);
-    return vector;
-}
 
-void print_vector(int *vector, int dim)
-{
     for (int i = 0; i < dim; i++)
     {
-        printf("%d ", vector[i]);
+
+        printf("%d ", v[i]);
     }
     printf("\n");
 }
 
-void swap(int *a, int *b)
-{
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
-void sort_vector(int *vector, int dim)
-{
-    for (int i = 0; i < dim - 1; i++)
-    {
-        for (int j = 0; j < dim - i - 1; j++)
-        {
-            if (vector[j] > vector[j + 1])
-            {
-                swap(&vector[j], &vector[j + 1]);
-            }
-        }
-    }
-}
-
-// Cerca il massimo locale escludendo elementi segnati da excluded_flags
-// e valori contenuti in common_values_to_exclude di dimensione exclude_count
-int find_max_element_in_array(int *array, int dim, int *excluded_flags, int *excluded_values)
-{
-    int max_val = -1; 
-
-    for (int i = 0; i < dim; i++)
-    {
-        if (excluded_flags[i]) // == 1 significa che il valore è da escludere
-            continue;
-
-        int skip = 0;
-        for (int j = 0; j < 5; j++) // 5 è il numero fisso di valori da escludere
-        {
-            if (array[i] == excluded_values[j])
-            {
-                skip = 1;
-                break;
-            }
-        }
-        if (skip) // == 1 significa che il valore è da escludere
-            continue;
-
-        if (array[i] > max_val)
-        {
-            max_val = array[i];
-        }
-    }
-
-    return max_val;
-}
 
 int main(int argc, char *argv[])
 {
@@ -107,117 +41,160 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int dim;
-    int *A = NULL;
+    int dim = DIM;
 
-    int k = 4; // elementi per processo
+    int k = 2;
 
-    if (rank == 0)
+    if ( dim % (k*size) != 0 )
     {
-        A = read_vector_from_file(FILE_NAME, &dim);
-        if (dim % (k * size) != 0)
-        {
-            fprintf(stderr, "Dimensione vettore non compatibile\n");
-            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-        }
-    }
-
-    MPI_Bcast(&dim, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    int *my_vector = malloc(k * sizeof(int));
-
-    MPI_Scatter(A, k, MPI_INT, my_vector, k, MPI_INT, 0, MPI_COMM_WORLD);
-        
-    printf("Process %d received vector: ", rank);
-    print_vector(my_vector, k);
-
-
-    sort_vector(my_vector, k);
-    printf("Process %d sorted vector: ", rank);
-    print_vector(my_vector, k);
-    
-
-    int *top_5_values = malloc(5 * sizeof(int));
-    if (rank == 0) {
-        // Inizializza top_5_values con valori impossibili
-        for (int i = 0; i < 5; i++)
-        {
-            top_5_values[i] = -1;
-        }
-    }
-
-    int excluded_flags[k];
-    for (int i = 0; i < k; i++)
-    {
-        excluded_flags[i] = 0;
-    }
-
-
-    int saved = 0;
-
-    for (int iter = 0; iter < 5; iter++)
-    {
-        int local_max = find_max_element_in_array(my_vector, k, excluded_flags, top_5_values);
-        int global_max;
-
-        // MPI_Reduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, 0, MPI_COMM_WORLD);
-        // MPI_Bcast(&global_max, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-        MPI_Allreduce(&local_max, &global_max, 1, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
-
         if (rank == 0)
-        {
-            int already_present = 0;
-            for (int i = 0; i < saved; i++)
-            {
-                if (top_5_values[i] == global_max)
-                {
-                    already_present = 1;
-                    break;
+            printf("dim must be multiple of k*nproc. change DIM or k.\n");
+        MPI_Finalize();
+        return -1;
+    }
+
+    int num_blocks = dim / k;
+    int blocks_per_proc = num_blocks / size;
+    int blocks_size = blocks_per_proc * k;
+
+    // create datatype for send all blocks to a process with a single send
+
+    MPI_Datatype blocks_type;
+    MPI_Type_vector(blocks_per_proc, k, size*k, MPI_INT, &blocks_type);
+    MPI_Type_commit(&blocks_type);
+
+    int *V = malloc(blocks_size * sizeof(int));
+
+    if (rank == 0) {
+
+        FILE *fp = fopen(FILE_NAME, "r");
+
+        if (fp == NULL){
+            perror("Error opening file");
+            MPI_Finalize();
+            return -1;
+        }
+
+        // proc0 legge dal file
+
+        int *A = malloc(dim * sizeof(int));
+
+        for (int i = 0; i < dim; i++){
+            fscanf(fp, "%d", &A[i]);
+        }
+
+        printf("\nVettore letto da file:\n");
+        print_vector(A, dim);
+        fflush(stdout);
+        fclose(fp);
+
+
+        // proc0 deve distribuire usando il datatype, ma prima
+        // deve copiarsi i suoi blocchi da tenere
+        for (int b = 0; b < num_blocks; b+=size){
+            for (int i = 0; i < k; i++){
+                int index = (b/size)*k + i;
+                V[index] = A[b*k+i];
+            }
+        }
+
+        printf("\nVettore dei blocchi di proc0:\n");
+        print_vector(V, blocks_size);
+        fflush(stdout);
+
+        // now proc0 can sends to others
+        for (int p = 1; p < size; p++) {
+            MPI_Send(A+p*k, 1, blocks_type, p, 0, MPI_COMM_WORLD);
+        }
+
+    } else {
+
+        MPI_Recv(V, blocks_per_proc *k, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        printf("\nVettore dei blocchi di proc%d:\n", rank);
+        print_vector(V, blocks_size);
+        fflush(stdout);
+
+    }
+
+    // ogni processo ordina il proprio V in senso crescente
+
+    for (int i = 0; i < (blocks_size)-1; i++){
+        for (int j = i+1; j < (blocks_size); j++) {
+
+            if (V[i] > V[j]) {
+
+                //swap
+                int temp = V[i];
+                V[i] = V[j];
+                V[j] = temp;
+            }
+        }
+    }
+
+    printf("\nVettore ordinato di proc%d:\n", rank);
+    print_vector(V, blocks_size);
+    fflush(stdout);
+
+    // we are gonna use this for computing max and remove max for next round
+    int *temp_V = malloc(blocks_size * sizeof(int));
+    for (int i = 0; i < blocks_size; i++){
+        temp_V[i] = V[i];
+    }
+
+    typedef struct {
+        int value;
+        int rank;
+    } maxloc_t;
+
+    maxloc_t local_max, global_max;
+
+
+    int *max_values = malloc(5 * sizeof(int));
+
+    for (int i = 0; i < 5; i++) {
+
+        // find local max
+        local_max.value = -1;
+        local_max.rank = rank;
+
+        for (int i = 0; i < blocks_size; i++) {
+            if (temp_V[i] > local_max.value) {
+                local_max.value = temp_V[i];
+            }
+        }
+
+        // un alternativa qui poteva essere fare reduce al proc0 solo del global_max, e
+        // poi proc0 doveva mandare il globalmax in bcast a tutti cosi gli altri possono escluderlo
+        MPI_Allreduce(&local_max, &global_max, 1, MPI_2INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+        if (rank == 0){
+
+            max_values[i] = global_max.value;
+        }
+
+        if (rank == global_max.rank) {
+            // this proc has the max, so we need to replace it in temp_V with INT32_MIN
+            for (int i = 0; i < blocks_size; i++){
+                if (temp_V[i] == global_max.value) {
+                    temp_V[i] = INT32_MIN;
                 }
             }
-
-            if (!already_present)
-            {
-                top_5_values[saved] = global_max;
-                saved++;
-                printf("Saved new value: %d\n", global_max);
-            }
-            else
-            {
-                printf("Duplicate value skipped: %d\n", global_max);
-                // Se è duplicato, decrementa iter per ripetere il passo e prendere un altro massimo
-                iter--;
-            }
         }
 
-        // Broadcast per aggiornare top_5_values a tutti, dato che solo il processo 0 lo aggiorna.
-        // Tutti i processi devono avere la stessa visione dei top 5 valori, perche devono usarlo per escludere i valori nei prossimi passi.
-        MPI_Bcast(top_5_values, 5, MPI_INT, 0, MPI_COMM_WORLD);
-
-        // Escludi localmente il valore trovato (anche se duplicato per evitare loop)
-        for (int j = 0; j < k; j++)
-        {
-            if (my_vector[j] == global_max)
-            {
-                excluded_flags[j] = 1;
-            }
-        }
     }
 
-    if (rank == 0)
-    {
-        printf("Top 5 values: ");
-        for (int i = 0; i < 5; i++)
-        {
-            printf("%d ", top_5_values[i]);
-        }
-        free(A);
-        free(top_5_values);
+    if (rank == 0){
+        printf("\n");
+        printf("Proc %d has max values vector:\n", rank);
+        print_vector(max_values, 5);
+        fflush(stdout);
     }
+
 
     printf("\n");
-    free(my_vector);
+    free(V);
+    MPI_Type_free(&blocks_type);
     MPI_Finalize();
     return 0;
 }
